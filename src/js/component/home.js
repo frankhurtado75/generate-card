9a7507ffae0a115cae2a7c497481fd388ebfac4e
frankhurtado75 <frankhurtado75@gmail.com>
2020-08-06 02:46:31 -0400

    update dinamic and bootstrap

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 //import { NewHome } from "./NewHome.js";
 import { Card } from "./Card";
 
@@ -15,70 +15,89 @@ export function Home() {
 	const [job, setJob] = useState("");
 	return (
 		<div className="row">
-			<label htmlFor="corporation">{"Work:"}</label>
-			<input
-				type="text"
-				name="corporation"
-				onChange={event => {
-					setCorporation(event.target.value);
-				}}
-				value={corporation}
-			/>
-			<label htmlFor="name">{"Name:"}</label>
-			<input
-				type="text"
-				name="name"
-				onChange={event => {
-					setName(event.target.value);
-				}}
-				value={name}
-			/>
-			<label htmlFor="fondo">{"Background:"}</label>
-			<input
-				type="text"
-				name="fondo"
-				onChange={event => {
-					setFondo(event.target.value);
-				}}
-				value={fondo}
-			/>
-			<label htmlFor="job">{"Job:"}</label>
-			<input
-				type="text"
-				name="job"
-				onChange={event => {
-					setJob(event.target.value);
-				}}
-				value={job}
-			/>
+			<div className="container">
+				<label htmlFor="corporation">{"Work:"}</label>
+				<input
+					type="text"
+					name="corporation"
+					onChange={event => {
+						setCorporation(event.target.value);
+					}}
+					value={corporation}
+				/>
+				<label htmlFor="name">{"Name:"}</label>
+				<input
+					type="text"
+					name="name"
+					onChange={event => {
+						setName(event.target.value);
+					}}
+					value={name}
+				/>
+				<label htmlFor="fondo">{"Background:"}</label>
+				<input
+					type="text"
+					name="fondo"
+					onChange={event => {
+						setFondo(event.target.value);
+					}}
+					value={fondo}
+				/>
+				<label htmlFor="job">{"Job:"}</label>
+				<input
+					type="text"
+					name="job"
+					onChange={event => {
+						setJob(event.target.value);
+					}}
+					value={job}
+				/>
 
-			<button
-				className="btn btn-primary"
-				onClick={e => {
-					//Hay que actualizar el estado people con los valores de los estados
-					let person = {
-						corporation: corporation,
-						name: name,
-						fondo: fondo,
-						job: job
-					};
-					setPeople([person]);
-				}}>
-				{"Add Card"}
-			</button>
-
-			{people.map((person, index) => {
-				//Renderiza Card para persona con nombre, fondo y job
-				return (
-					<Card
-						key={index}
-						corporation={person.corporation}
-						name={person.name}
-						fondo={person.fondo}
-						job={person.job}
-					/>
-				);
-			})}
+				<button
+					className="btn btn-primary"
+					onClick={e => {
+						//Hay que actualizar el estado people con los valores de los estados
+						let newPerson = {
+							corporation: corporation,
+							name: name,
+							fondo: fondo,
+							job: job
+						};
+						/*
+					//Copiar people
+					let newPeople = [];
+					for (let person of people) {
+						newPeople.push(person);
+					}
+					newPeople.push(newPerson);
+					//Agregar nueva persona a la copia
+					//setPeople de la copia
+					setPeople(newPeople);
+					*/
+						//Spread operator ...
+						setPeople([...people, newPerson]);
+						setCorporation("");
+						setName("");
+						setFondo("");
+						setJob("");
+					}}>
+					{"Add Card"}
+				</button>
+			</div>
+			<div className="row">
+				{people.map((person, index) => {
+					//Renderiza Card
+					return (
+						<Card
+							key={index}
+							corporation={person.corporation}
+							name={person.name}
+							fondo={person.fondo}
+							job={person.job}
+						/>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
